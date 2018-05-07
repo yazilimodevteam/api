1502b4363518d46ca510cd83771fd659569a4d5b
celal258 <celal258@gmail.com>
2018-05-07 23:43:48 +0300

    first

--- src/app.js
+++ src/app.js
@@ -4,28 +4,12 @@ import cors from 'cors';
 import io from 'socket.io';
 import config from '../config/config.json';
 import path from 'path';
-import myysql from 'mysql';
-
 
 // setup server
 const app = express();
 const server = http.createServer(app);
-const { Pool, Client } = require('pg')
+
 const socketIo = io(server);
-
-const client = new Client({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'realtime_message',
-  password: 'qwer1234',
-  port: 5432,
-})
-
-con.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
-});
-
 
 // Allow CORS
 app.use(cors());
@@ -35,16 +19,6 @@ app.get('/', (req, res) => {
   res.sendFile(path.resolve('public/index.html'));
 });
 
-//İlk 
-
-app.route('/message')
-		.get(function(req, res) {
-		  db.query("SELECT * FROM message", function (err, result, fields) {
-		    if (err) throw err;
-		    res.json(result);
-		    console.log(result);
-		  })
-		});
 // Start listening
 server.listen(process.env.PORT || config.port);
 console.log(`Started on port ${config.port}`);
@@ -57,10 +31,6 @@ socketIo.on('connection', socket => {
   socket.on('client:message', data => {
     console.log(`${data.username}: ${data.message}`);
 
-	client.query("INSERT INTO \"message\" (\"user\", \"mesaj\") VALUES ('"+data.username+"', '"+data.message+"')", function (err, result) {
-		    if (err) throw err;
-	    console.log("1 record inserted");
-	  });
     // message received from client, now broadcast it to everyone else
     socket.broadcast.emit('server:message', data);
   });
