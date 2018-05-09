ee81b7dbf1a029e2c693e8efd425533e6f78a50c
celal258 <celal258@gmail.com>
2018-05-09 08:34:56 +0300

    kullaniciGirisi baslangic

--- src/app.js
+++ src/app.js
@@ -5,6 +5,9 @@ import io from 'socket.io';
 import config from '../config/config.json';
 import path from 'path';
 import postgre from 'pg';
+import session  from "express-session";
+import bodyParser from 'body-parser';
+import parseFormdata from 'parse-formdata'
 
 // setup server
 const app = express();
@@ -19,7 +22,7 @@ const { Pool, Client } = postgre
 
 const client = new Client({
   user: 'postgres',
-  host: '192.168.0.104',
+  host: 'localhost',
   database: 'realtime_message',
   password: 'qwer1234',
   port: 5432,
@@ -30,17 +33,58 @@ client.connect(function (err) {
   console.log("Connected!")
 });
 
+/** bodyParser.urlencoded(options)
+ * Parses the text as URL encoded data (which is how browsers tend to send form data from regular forms set to POST)
+ * and exposes the resulting object (containing the keys and values) on req.body
+ */
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+/**bodyParser.json(options)
+ * Parses the text as JSON and exposes the resulting object on req.body.
+ */
+app.use(bodyParser.json());
+
+app.use(session({
+  secret: "Özel-Anahtar",
+  resave: false,
+  saveUninitialized: true
+}));
+
 //İlk 
 
 app.route('/message')
 		.get(function(req, res) {
-      client.query("SELECT * FROM message ORDER BY id DESC LIMIT 50", function (err, result, fields) {
-		    if (err) throw err;
-		    res.json(result);
-		    console.log(result);
-		  })
-		});
-
+      /*if (req.session.login)
+      {*/
+        client.query("SELECT * FROM message ORDER BY \"Id\" DESC LIMIT 50", function (err, result, fields) {
+          if (err) throw err;
+          res.json(result);
+          //console.log(result);
+        });
+      /*}
+      else
+      {
+        res.send("Kullanıcı girişi yapılmadı.");
+      }*/
+      
+    });
+app.route('/login')
+  .get(function (req, res) {
+    
+    if (req.session.login) return res.send(req.session.login);
+    res.send("Session bulunamadı");
+  })
+  .post(function (req, res) {
+    parseFormdata(req, function (err, data) {
+      if (err) throw err
+      var user_name = data.fields.user;
+      var password = data.fields.password;
+      console.log("Login Successful")
+      console.log("User name = " + user_name + ", password is " + password);
+    })
+  })
 // Start listening
 // Render a API index page
 app.get('/', (req, res) => {
@@ -51,6 +95,7 @@ app.get('/', (req, res) => {
 // Start listening
 server.listen(process.env.PORT || config.port);
 console.log(`Started on port ${config.port}`);
+
 
 // Setup socket.io
 socketIo.on('connection', socket => {
